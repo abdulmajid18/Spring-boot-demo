865803c2dd06a53a44c2ea4e6bf9cf61b89323d6
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-02-18 22:14:49 +0000

    update

--- src/main/java/com/rozz/CodeDI/CartSerice1.java
+++
@@ -1,9 +0,0 @@
-package com.rozz.CodeDI;
-
-public class CartSerice1 {
-    private CartReository repository;
-
-    public CartSerice1() {
-        this.repository = new CartRepositoryImpl();
-    }
-}
--- src/main/java/com/rozz/CodeDI/CartService2.java
+++
@@ -1,9 +0,0 @@
-package com.rozz.CodeDI;
-
-public class CartService2 {
-    private CartRepository repository;
-
-    public CartService2(CartRepository repository) {
-        this.repository = repository;
-    }
-}
