b68f1a77331bb3e9c3874ca18bd34e1ebe1a46ac
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-02-27 23:25:50 +0000

    added OptionCount dto

---
+++ src/main/java/com/rozz/Dto/OptionCount.java
@@ -0,0 +1,22 @@
+package com.rozz.Dto;
+
+public class OptionCount {
+    private Long optionId;
+    private int count;
+
+    public void setOptionId(Long optionId) {
+        this.optionId = optionId;
+    }
+
+    public Long getOptionId() {
+        return optionId;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
