74343c30a2c4f79da1394421ba26639e3854d244
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-02-27 23:24:56 +0000

    added getters and setters

--- src/main/java/com/rozz/Domain/Vote.java
+++ src/main/java/com/rozz/Domain/Vote.java
@@ -17,4 +17,23 @@ public class Vote {
     @ManyToOne
     @JoinColumn(name = "OPTION_ID")
     private Option options;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Option getOptions() {
+        return options;
+
+    }
+
+    public void setOptions(Option options) {
+        this.options = options;
+
+    }
+
 }
