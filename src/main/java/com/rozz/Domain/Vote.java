d32ea7e7e829e6c86abefa0f5c5a0f571efbcf90
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-02-27 22:17:43 +0000

    add domain model- Vote

---
+++ src/main/java/com/rozz/Domain/Vote.java
@@ -0,0 +1,20 @@
+package com.rozz.Domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Vote {
+    @Id
+    @GeneratedValue
+    @Column(name = "VOTE_ID")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "OPTION_ID")
+    private Option options;
+}
