24dff2cda8a4ce5def4952b9a34d6da4acd445aa
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-02-27 22:18:12 +0000

    Option Repo

---
+++ src/main/java/com/rozz/Repository/OptionRepository.java
@@ -0,0 +1,9 @@
+package com.rozz.Repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.rozz.Domain.Option;
+
+public interface OptionRepository extends CrudRepository<Option, Long> {
+
+}
